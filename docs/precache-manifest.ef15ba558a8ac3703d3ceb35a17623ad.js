ca09ace75a0cf726344e1b51b677403cdf3254ff
Eriold <dacamoty@gmail.com>
2020-10-17 20:18:59 -0500

    add docs

---
+++ docs/precache-manifest.ef15ba558a8ac3703d3ceb35a17623ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a6144c60caa545ef7cb123ac142c46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58e661ba1a518dec473f",
+    "url": "/static/css/main.610c18d0.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e661ba1a518dec473f",
+    "url": "/static/js/main.0fd3cdb6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
